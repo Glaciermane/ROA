4eb2fadb0f88c313b5c503aef048af9cee2f8e5f
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-06-26 07:36:32 +0200

    Create loader.sh

---
+++ loader.sh
@@ -0,0 +1,90 @@
+# loader
+color b
+echo "	 OOOOOOOOOO             "
+echo "OOOOOOOOOOOO     OOO      "
+echo "OOO       OOO    O        " 
+echo "OO          OO            "
+echo "		OOO       OOOO      "
+echo "OOOOOOOOOOOO              "
+echo "		OOOOOO     OOOOOO   "   
+echo " OOO          OOOO        "
+echo " OOO   	OO         OOO  "   
+echo " OOO  OO	           OOO  "  
+echo "  OOO            OOO      "
+echo "   OOOO        OOOO       "
+echo "  	   OOOOOOOOOOOO     " 
+echo "      OOOOOOOO            "
+sleep 1
+cls
+
+echo " OOOOOOOOOO      	  	O   OOOOOOOOOOOOOOO            "
+echo "OOOOOOOOOOOO        OO	 	OOOOOOOOOOOOOOOOOO         "
+echo "OOO       OOO      OOOOOOOOOO      OOOO              "
+echo "OO          OO     	OOOOOOO           OO             "
+echo "OOO       OOOO			 OOOOOOO                         "
+echo "OOOOOOOOOOOO       	 OOOOOOOOO          OO           "
+echo "OOOOOO     OOOOOO   OO	  	 OOOOOOOOOOOOOO          "
+echo " OOO          OOOO         OOOOOOOOOOOOOOOOO         "
+echo " OOO            OOO    	  OOOOO        OO            "
+echo " OOO             OOO        O OOOOOOO                " 
+echo "  OOO            OOO       	   OOOOOO                " 
+echo "   OOOO        OOOO           OOOOOOOOOO       OOO   " 
+echo "     OOOOOOOOOOOO      	    	OOOOOOOOOOOOOOOOOO     "
+echo "      OOOOOOOO	      OOOOOOOOOOOOOO                 "
+sleep 1
+cls
+
+
+echo " OOOOOOOOOO                  OOOOOOOOOOOOOOO       	       OOOOOOO		         "
+echo "OOOOOOOOOOOO                OOOOOOOOOOOOOOOOOO          OOOOO   OOOO	         "
+echo "OOO       OOO              OOOOOOOOOO      OOOO        OOOO        OOO	       "
+echo "OO          OO             OOOOOOO                    OOO            OO	       "
+echo "OOO       OOOO              OOOOOOO                 	  OOO            OO 	   "
+echo "OOOOOOOOOOOO                OOOOOOOOO                 OOO      OOOO  OO	       "
+echo "OOOOOO     OOOOOO           OOOOOOOOOOOOOO            OOOO  OOOO   OOOO 	     "
+echo " OOO          OOOO           OOOOOOOOOOOOOOOOO        OOOOOO        OO 	       "
+echo " OOO            OOO          OOOOO                  	   OOO          OO	     "
+echo " OOO             OOO         OOOOOOO              	     OOO         OO  	     "
+echo "  OOO            OOO          OOOOOO                   OO          OO	         "
+echo "   OOOO        OOOO           OOOOOOOOOO       OOO     OO         OO	         "
+echo "     OOOOOOOOOOOO              OOOOOOOOOOOOOOOOOO   	  OO         OOO 	       "
+echo "      OOOOOOOO                   OOOOOOOOOOOOOO      OO           OOO 	       "
+sleep 1
+cls
+
+
+echo " OOOOOOOOOO      	          OOOOOOOOOOOOOOO              OOOOOOO		OO	O    OOOOOOOOOO		         "
+echo "OOOOOOOOOOOO        	 OOOOOOOOOOOOOOOOOO          OOOOO   OOOO	 OOOOOOOOOOOOOO  	                 "
+echo "OOO       OOO      	OOOOOOOOOO      OOOO        OOOO        OOO	OOOOOOO       OO	                 "
+echo "OO          OO     	OOOOOOO                    OOO            OO		OOO			                       "
+echo "OOO       OOOO		 OOOOOOO                   OOO            OO 	OOO   		O	                       "
+echo "OOOOOOOOOOOO       	 OOOOOOOOO                 OOO      OOOO  OO	 OOO   		OO	                   "
+echo "OOOOOO     OOOOOO     	 OOOOOOOOOOOOOO            OOOO  OOOO   OOOO 	 	 OOOOOOOOOOOO		           "
+echo " OOO          OOOO           OOOOOOOOOOOOOOOOO        OOOOOO        OO 	    OOOOOOOOOOOO	         "
+echo " OOO            OOO    	  OOOOO                     OOO          OO	OO             OOOO	           "
+echo " OOO             OOO         OOOOOOO                   OOO         OO  	  	 OO           OOO	     " 
+echo "  OOO            OOO          OOOOOO                    OO          OO	       OO       OOO	       "
+echo "   OOOO        OOOO           OOOOOOOOOO       OOO     OO         OO	  OO      OOOOOO	           "
+echo "     OOOOOOOOOOOO      	    OOOOOOOOOOOOOOOOOO     OO         OOO 	  	 OOOOOOOOOOOO		           "
+echo "      OOOOOOOO		      OOOOOOOOOOOOOO      OO           OOO 	     OOOOOOOO		                     "
+sleep 1
+cls
+
+
+echo " OOOOOOOOOO               OOOOOOOOOOOOOOO              OOOOOOO             OOOOOOOOOO          OOOOOOOOOOOOO         "
+echo "OOOOOOOOOOOO             OOOOOOOOOOOOOOOOOO          OOOOO   OOOO        OOOOOOOOOOOOOO      OOOO     OO   OOO       "
+echo "OOO       OOO           OOOOOOOOOO      OOOO        OOOO        OOO     OOOOOOO       OO      OO       OO    OOO     "
+echo "OO          OO          OOOOOOO                    OOO            OO    OOO                            OO     O      "
+echo "OOO       OOOO           OOOOOOO                   OOO            OO    OOO                            OOO           "
+echo "OOOOOOOOOOOO             OOOOOOOOO                 OOO      OOOO  OO     OOO                          OOOO           "
+echo "OOOOOO     OOOOOO        OOOOOOOOOOOOOO            OOOO  OOOO   OOOO      OOOOOOOOOOOO                OOO            "
+echo " OOO          OOOO        OOOOOOOOOOOOOOOOO        OOOOOO        OO         OOOOOOOOOOOO              OOO            "
+echo " OOO            OOO       OOOOO                     OOO          OO                  OOOO            OOO             "
+echo " OOO             OOO      OOOOOOO                   OOO         OO                    OOO           OOO              "
+echo "  OOO            OOO       OOOOOO                    OO         OO                    OOO          OOO               "
+echo "   OOOO        OOOO         OOOOOOOOOO       OOO     OO         OO        OO      OOOOOO           OOO               "
+echo "     OOOOOOOOOOOO            OOOOOOOOOOOOOOOOOO     OO          OOO        OOOOOOOOOOOO            OOOO              "
+echo "      OOOOOOOO                 OOOOOOOOOOOOOO      OO            OOO         OOOOOOOO               OO               "
+sleep 2
+
+
