4a8364fc9dc8f70dfc689be8f0290095fd4284b5
Zest <89149495+Glaciermane@users.noreply.github.com>
2023-06-26 07:59:52 +0200

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -1,90 +1,97 @@
 # loader
 color b
-echo -e "\[95m	 OOOOOOOOOO             "
-echo -e "\[95m OOOOOOOOOOOO     OOO      "
-echo -e "\[95m OOO       OOO    O        " 
-echo -e "\[95m OO          OO            "
-echo -e "\[95m 		OOO       OOOO      "
-echo -e "\[95m OOOOOOOOOOOO              "
-echo -e "\[95m 		OOOOOO     OOOOOO   "   
-echo -e "\[95m  OOO          OOOO        "
-echo -e "\[95m  OOO   	OO         OOO  "   
-echo -e "\[95m  OOO  OO	           OOO  "  
-echo -e "\[95m   OOO            OOO      "
-echo -e "\[95m    OOOO        OOOO       "
-echo -e "\[95m   	   OOOOOOOOOOOO     " 
-echo -e "\[95m       OOOOOOOO            \e[0m"
+echo -e "$(tput setaf 5)
+ 	 OOOOOOOOOO             \n
+  OOOOOOOOOOOO     OOO      \n
+  OOO       OOO    O        \n
+  OO          OO            \n
+  		OOO       OOOO      \n
+  OOOOOOOOOOOO             \n
+ 		OOOOOO     OOOOOO     \n
+   OOO          OOOO       \n
+   OOO   	OO         OOO    \n
+   OOO  OO	           OOO   \n
+    OOO            OOO      \n
+     OOOO        OOOO       \n
+    	   OOOOOOOOOOOO     \n
+        OOOOOOOO        \n
+$(tput sgr0)"
 sleep 1
 clear
 
-echo " OOOOOOOOOO      	  	O   OOOOOOOOOOOOOOO            "
-echo "OOOOOOOOOOOO        OO	 	OOOOOOOOOOOOOOOOOO         "
-echo "OOO       OOO      OOOOOOOOOO      OOOO              "
-echo "OO          OO     	OOOOOOO           OO             "
-echo "OOO       OOOO			 OOOOOOO                         "
-echo "OOOOOOOOOOOO       	 OOOOOOOOO          OO           "
-echo "OOOOOO     OOOOOO   OO	  	 OOOOOOOOOOOOOO          "
-echo " OOO          OOOO         OOOOOOOOOOOOOOOOO         "
-echo " OOO            OOO    	  OOOOO        OO            "
-echo " OOO             OOO        O OOOOOOO                " 
-echo "  OOO            OOO       	   OOOOOO                " 
-echo "   OOOO        OOOO           OOOOOOOOOO       OOO   " 
-echo "     OOOOOOOOOOOO      	    	OOOOOOOOOOOOOOOOOO     "
-echo "      OOOOOOOO	      OOOOOOOOOOOOOO                 "
+echo -e "$(tput setaf 5)
+  OOOOOOOOOO      	  	O   OOOOOOOOOOOOOOO            \n
+ OOOOOOOOOOOO        OO	 	OOOOOOOOOOOOOOOOOO         \n
+ OOO       OOO      OOOOOOOOOO      OOOO              \n
+ OO          OO     	OOOOOOO           OO             \n
+ OOO       OOOO			 OOOOOOO                         \n
+ OOOOOOOOOOOO       	 OOOOOOOOO          OO           \n
+ OOOOOO     OOOOOO   OO	  	 OOOOOOOOOOOOOO          \n
+  OOO          OOOO         OOOOOOOOOOOOOOOOO         \n
+  OOO            OOO    	  OOOOO        OO            \n
+  OOO             OOO        O OOOOOOO                 \n
+   OOO            OOO       	   OOOOOO                 \n
+    OOOO        OOOO           OOOOOOOOOO       OOO    \n
+      OOOOOOOOOOOO      	    	OOOOOOOOOOOOOOOOOO     \n
+       OOOOOOOO	      OOOOOOOOOOOOOO                    \n
+$(tput sgr0)"
 sleep 1
 clear
 
-
-echo " OOOOOOOOOO                  OOOOOOOOOOOOOOO       	       OOOOOOO		         "
-echo "OOOOOOOOOOOO                OOOOOOOOOOOOOOOOOO          OOOOO   OOOO	         "
-echo "OOO       OOO              OOOOOOOOOO      OOOO        OOOO        OOO	       "
-echo "OO          OO             OOOOOOO                    OOO            OO	       "
-echo "OOO       OOOO              OOOOOOO                 	  OOO            OO 	   "
-echo "OOOOOOOOOOOO                OOOOOOOOO                 OOO      OOOO  OO	       "
-echo "OOOOOO     OOOOOO           OOOOOOOOOOOOOO            OOOO  OOOO   OOOO 	     "
-echo " OOO          OOOO           OOOOOOOOOOOOOOOOO        OOOOOO        OO 	       "
-echo " OOO            OOO          OOOOO                  	   OOO          OO	     "
-echo " OOO             OOO         OOOOOOO              	     OOO         OO  	     "
-echo "  OOO            OOO          OOOOOO                   OO          OO	         "
-echo "   OOOO        OOOO           OOOOOOOOOO       OOO     OO         OO	         "
-echo "     OOOOOOOOOOOO              OOOOOOOOOOOOOOOOOO   	  OO         OOO 	       "
-echo "      OOOOOOOO                   OOOOOOOOOOOOOO      OO           OOO 	       "
+echo -e "$(tput setaf 5)
+  OOOOOOOOOO                  OOOOOOOOOOOOOOO       	       OOOOOOO		         \n
+ OOOOOOOOOOOO                OOOOOOOOOOOOOOOOOO          OOOOO   OOOO	         \n
+ OOO       OOO              OOOOOOOOOO      OOOO        OOOO        OOO	       \n
+ OO          OO             OOOOOOO                    OOO            OO	       \n
+ OOO       OOOO              OOOOOOO                 	  OOO            OO 	   \n
+ OOOOOOOOOOOO                OOOOOOOOO                 OOO      OOOO  OO	       \n
+ OOOOOO     OOOOOO           OOOOOOOOOOOOOO            OOOO  OOOO   OOOO 	     \n
+  OOO          OOOO           OOOOOOOOOOOOOOOOO        OOOOOO        OO 	       \n
+  OOO            OOO          OOOOO                  	   OOO          OO	     \n
+  OOO             OOO         OOOOOOO              	     OOO         OO  	     \n
+   OOO            OOO          OOOOOO                   OO          OO	         \n
+    OOOO        OOOO           OOOOOOOOOO       OOO     OO         OO	         \n
+      OOOOOOOOOOOO              OOOOOOOOOOOOOOOOOO   	  OO         OOO 	       \n
+       OOOOOOOO                   OOOOOOOOOOOOOO      OO           OOO 	       \n
+$(tput sgr0)"
 sleep 1
 clear
 
-
-echo " OOOOOOOOOO      	          OOOOOOOOOOOOOOO              OOOOOOO		OO	O    OOOOOOOOOO		         "
-echo "OOOOOOOOOOOO        	 OOOOOOOOOOOOOOOOOO          OOOOO   OOOO	 OOOOOOOOOOOOOO  	                 "
-echo "OOO       OOO      	OOOOOOOOOO      OOOO        OOOO        OOO	OOOOOOO       OO	                 "
-echo "OO          OO     	OOOOOOO                    OOO            OO		OOO			                       "
-echo "OOO       OOOO		 OOOOOOO                   OOO            OO 	OOO   		O	                       "
-echo "OOOOOOOOOOOO       	 OOOOOOOOO                 OOO      OOOO  OO	 OOO   		OO	                   "
-echo "OOOOOO     OOOOOO     	 OOOOOOOOOOOOOO            OOOO  OOOO   OOOO 	 	 OOOOOOOOOOOO		           "
-echo " OOO          OOOO           OOOOOOOOOOOOOOOOO        OOOOOO        OO 	    OOOOOOOOOOOO	         "
-echo " OOO            OOO    	  OOOOO                     OOO          OO	OO             OOOO	           "
-echo " OOO             OOO         OOOOOOO                   OOO         OO  	  	 OO           OOO	     " 
-echo "  OOO            OOO          OOOOOO                    OO          OO	       OO       OOO	       "
-echo "   OOOO        OOOO           OOOOOOOOOO       OOO     OO         OO	  OO      OOOOOO	           "
-echo "     OOOOOOOOOOOO      	    OOOOOOOOOOOOOOOOOO     OO         OOO 	  	 OOOOOOOOOOOO		           "
-echo "      OOOOOOOO		      OOOOOOOOOOOOOO      OO           OOO 	     OOOOOOOO		                     "
+echo -e "$(tput setaf 5)
+  OOOOOOOOOO      	          OOOOOOOOOOOOOOO              OOOOOOO		OO	O    OOOOOOOOOO		         \n
+ OOOOOOOOOOOO        	 OOOOOOOOOOOOOOOOOO          OOOOO   OOOO	 OOOOOOOOOOOOOO  	                 \n
+ OOO       OOO      	OOOOOOOOOO      OOOO        OOOO        OOO	OOOOOOO       OO	                 \n
+ OO          OO     	OOOOOOO                    OOO            OO		OOO			                       \n
+ OOO       OOOO		 OOOOOOO                   OOO            OO 	OOO   		O	                       \n
+ OOOOOOOOOOOO       	 OOOOOOOOO                 OOO      OOOO  OO	 OOO   		OO	                   \n
+ OOOOOO     OOOOOO     	 OOOOOOOOOOOOOO            OOOO  OOOO   OOOO 	 	 OOOOOOOOOOOO		           \n
+  OOO          OOOO           OOOOOOOOOOOOOOOOO        OOOOOO        OO 	    OOOOOOOOOOOO	         \n
+  OOO            OOO    	  OOOOO                     OOO          OO	OO             OOOO	           \n
+  OOO             OOO         OOOOOOO                   OOO         OO  	  	 OO           OOO	      \n
+   OOO            OOO          OOOOOO                    OO          OO	       OO       OOO	       \n
+    OOOO        OOOO           OOOOOOOOOO       OOO     OO         OO	  OO      OOOOOO	           \n
+      OOOOOOOOOOOO      	    OOOOOOOOOOOOOOOOOO     OO         OOO 	  	 OOOOOOOOOOOO		           \n
+       OOOOOOOO		      OOOOOOOOOOOOOO      OO           OOO 	     OOOOOOOO		                        \n
+$(tput sgr0)"
 sleep 1
 clear
 
-
-echo " OOOOOOOOOO               OOOOOOOOOOOOOOO              OOOOOOO             OOOOOOOOOO          OOOOOOOOOOOOO         "
-echo "OOOOOOOOOOOO             OOOOOOOOOOOOOOOOOO          OOOOO   OOOO        OOOOOOOOOOOOOO      OOOO     OO   OOO       "
-echo "OOO       OOO           OOOOOOOOOO      OOOO        OOOO        OOO     OOOOOOO       OO      OO       OO    OOO     "
-echo "OO          OO          OOOOOOO                    OOO            OO    OOO                            OO     O      "
-echo "OOO       OOOO           OOOOOOO                   OOO            OO    OOO                            OOO           "
-echo "OOOOOOOOOOOO             OOOOOOOOO                 OOO      OOOO  OO     OOO                          OOOO           "
-echo "OOOOOO     OOOOOO        OOOOOOOOOOOOOO            OOOO  OOOO   OOOO      OOOOOOOOOOOO                OOO            "
-echo " OOO          OOOO        OOOOOOOOOOOOOOOOO        OOOOOO        OO         OOOOOOOOOOOO              OOO            "
-echo " OOO            OOO       OOOOO                     OOO          OO                  OOOO            OOO             "
-echo " OOO             OOO      OOOOOOO                   OOO         OO                    OOO           OOO              "
-echo "  OOO            OOO       OOOOOO                    OO         OO                    OOO          OOO               "
-echo "   OOOO        OOOO         OOOOOOOOOO       OOO     OO         OO        OO      OOOOOO           OOO               "
-echo "     OOOOOOOOOOOO            OOOOOOOOOOOOOOOOOO     OO          OOO        OOOOOOOOOOOO            OOOO              "
-echo "      OOOOOOOO                 OOOOOOOOOOOOOO      OO            OOO         OOOOOOOO               OO               "
+echo -e "$(tput setaf 5)
+  OOOOOOOOOO               OOOOOOOOOOOOOOO              OOOOOOO             OOOOOOOOOO          OOOOOOOOOOOOO           \n  
+ OOOOOOOOOOOO             OOOOOOOOOOOOOOOOOO          OOOOO   OOOO        OOOOOOOOOOOOOO      OOOO     OO   OOO         \n
+ OOO       OOO           OOOOOOOOOO      OOOO        OOOO        OOO     OOOOOOO       OO      OO       OO    OOO        \n
+ OO          OO          OOOOOOO                    OOO            OO    OOO                            OO     O         \n 
+ OOO       OOOO           OOOOOOO                   OOO            OO    OOO                            OOO           \n
+ OOOOOOOOOOOO             OOOOOOOOO                 OOO      OOOO  OO     OOO                          OOOO           \n
+ OOOOOO     OOOOOO        OOOOOOOOOOOOOO            OOOO  OOOO   OOOO      OOOOOOOOOOOO                OOO            \n
+  OOO          OOOO        OOOOOOOOOOOOOOOOO        OOOOOO        OO         OOOOOOOOOOOO              OOO            \n
+  OOO            OOO       OOOOO                     OOO          OO                  OOOO            OOO             \n
+  OOO             OOO      OOOOOOO                   OOO         OO                    OOO           OOO              \n
+   OOO            OOO       OOOOOO                    OO         OO                    OOO          OOO               \n
+    OOOO        OOOO         OOOOOOOOOO       OOO     OO         OO        OO      OOOOOO           OOO               \n
+      OOOOOOOOOOOO            OOOOOOOOOOOOOOOOOO     OO          OOO        OOOOOOOOOOOO            OOOO              \n
+       OOOOOOOO                 OOOOOOOOOOOOOO      OO            OOO         OOOOOOOO               OO               \n
+$(tput sgr0)"
 sleep 2
 
 
